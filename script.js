edb246654f0e79176787fbaca1e37262d433fe36
Mostafa <m.alsham627@gmail.com>
2020-11-09 20:05:23 -0700

    assigned variables to HTML elements

---
+++ script.js
@@ -0,0 +1,6 @@
+//assigning variables to HTML elements, in order to be called later on in the script. 
+const beginBtn = document.getElementById("begin");
+const beginQuiz = document.getElementById("begin-quiz");
+const questionBox = document.getElementById("question-box");
+const questionText = document.getElementById("question-text");
+const answerDiv = document.getElementById("answers");
