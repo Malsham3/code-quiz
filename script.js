7c69408a2c5d66cfcef59998eeb6f7581c3f7c72
Mostafa <m.alsham627@gmail.com>
2020-11-09 20:12:41 -0700

    initialized a questions array, will modify

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ const beginQuiz = document.getElementById("begin-quiz");
 const questionBox = document.getElementById("question-box");
 const questionText = document.getElementById("question-text");
 const answerDiv = document.getElementById("answers");
+
+let questionIndex = 0;
+
+const questions = [
+    {
+        question: "Question 1",
+        answers: ["True", "False"],
+        answerIndex: 0
+    },
+    {
+        question: "Question 2",
+        answers: ["True", "False"],
+        answerIndex: 0
+    }
+    // edit previous questions
+    // write more questions. 
+]
