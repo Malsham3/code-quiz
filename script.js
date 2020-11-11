99a33f5124d5f1c4625a4d8086c2a18e51f18334
Mostafa <m.alsham627@gmail.com>
2020-11-10 22:44:42 -0700

    initials and scores are now saved in localStorage

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const feedbackMsg = document.getElementById("feedback-msg");
 const countDown = document.getElementById("time");
 const resultField = document.getElementById("result-field")
 const scoreText = document.getElementById("score-text");
+const submitBtn = document.getElementById("submit");
+const highScoresField = document.getElementById("score-keeper")
 
 let questionIndex = 0;
 
@@ -26,11 +28,11 @@ const questions = [
         question: "The sun is a planet.",
         answers: ["True", "False"],
         answerIndex: 1
-    },{
+    }, {
         question: "What is the most famous Mexican beer?",
         answers: ["Corona", "Modelo", "Dos Equis"],
         answerIndex: 0
-    },{
+    }, {
         question: "What is the name of Apple's virtual assistant?",
         answers: ["Alexa", "Cortana", "Siri", "Lady Gaga"],
         answerIndex: 2
@@ -51,8 +53,26 @@ beginBtn.addEventListener("click", function (e) {
 
 });
 
+
+submitBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    var initials = document.getElementById("initials-form").value;
+    //get initials
+    var score = finalScore;
+    localStorage.setItem(initials, score);
+
+    //this for-loop is going to create a new row for each time initials submitted to keep scores data.
+    for (let i = 0; i < localStorage.length; i++) {
+        const answer = currentQuestion.answers[i];
+        const answerBtn = document.createElement("button");
+        answerBtn.setAttribute("class", "btn btn-primary mx-1");
+        answerBtn.textContent = answer;
+        answerDiv.appendChild(answerBtn);
+
+    }
+})
+
 let finalScore = 0;
-var initials = '';
 answerDiv.addEventListener("click", function (e) {
     e.preventDefault();
     //no action taken if click wasn't on a button. 
@@ -79,7 +99,7 @@ answerDiv.addEventListener("click", function (e) {
     if (questionIndex < questions.length) {
         newQuestion();
     } else {
-        saveScore(finalScore);
+        alert("Game over!")
         displayResults();
     }
 });
@@ -94,14 +114,14 @@ function newQuestion() {
     for (let i = 0; i < currentQuestion.answers.length; i++) {
         const answer = currentQuestion.answers[i];
         const answerBtn = document.createElement("button");
-        answerBtn.setAttribute("class", "btn btn-primary");
+        answerBtn.setAttribute("class", "btn btn-primary mx-1");
         answerBtn.textContent = answer;
         answerDiv.appendChild(answerBtn);
 
     }
 }
 
-var secondsLeft = 60; 
+var secondsLeft = 60;
 function setTime() {
     var timerInterval = setInterval(function () {
         secondsLeft--;
@@ -114,21 +134,14 @@ function setTime() {
 }
 
 function displayResults() {
-
-    alert("Game over!")
+    scoreText.textContent = finalScore;
     beginQuiz.style.display = "none";
     questionField.style.display = "none";
     resultField.style.display = "block";
-    scoreText.textContent = finalScore;
-    //display initials form and take initials as input and save in local storage.
 }
 
-// function that saves final score and saves it into local storage.
-function saveScore(score){
-    localStorage.setItem("initials", finalScore);
-}
 
 
 
 //HTML: add a form that asks for initials, in JS, add event listener that saves input
-//ask to get initials and use saveScore to save
+//ask to get initials and use saveInfo to save
